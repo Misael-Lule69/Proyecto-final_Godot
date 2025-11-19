94c349f46458f6daf4b4f7656f2f3b9cabff5edb
Misael-Lule69 <misaellule123@gmail.com>
2025-11-18 22:59:27 -0600

    Cambio de interfaz

--- Pantalla_pausa/pantalla_pausa.gd
+++ Pantalla_pausa/pantalla_pausa.gd
@@ -1,22 +1,22 @@
-extends Node2D
+extends Control  # <--- CAMBIO IMPORTANTE: De Node2D a Control
 
 func _on_resume_pressed() -> void:
-		# Ocultar el menú de pausa
+	# Ocultar el menú de pausa
 	hide()
 	# Reanudar el juego (quitar la pausa)
 	get_tree().paused = false
 
-
 func _on_restart_pressed() -> void:
-		# Antes de cambiar de escena, quitar la pausa
+	# Antes de cambiar de escena, quitar la pausa es vital
 	get_tree().paused = false
+	
 	if Global.scene != "":
 		get_tree().change_scene_to_file(Global.scene)
 	else:
-		get_tree().change_scene_to_file("res://nivel1.tscn")
-
+		# Asegúrate de que esta ruta sea correcta
+		get_tree().change_scene_to_file("res://nivel1.tscn") 
 
 func _on_quit_pressed() -> void:
-		# Antes de cambiar de escena, quitar la pausa
+	# Antes de cambiar de escena, quitar la pausa
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Menu/pantalla_Menu.tscn")
