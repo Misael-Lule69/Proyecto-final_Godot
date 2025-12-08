33ecbe2ff4469438194341a4d44db2efe1a42286
AldoUriel09 <acevedoaldo492@gmail.com>
2025-12-07 23:56:49 -0600

    Nivel4_final

--- Personaje/character_body_2d.gd
+++ Personaje/character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 180.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -450.0
 const DEATH_HEIGHT = 1000
 const GRAVITY = 980 
 
--- puerta_fin_nivel/puerta.gd
+++ puerta_fin_nivel/puerta.gd
@@ -1,39 +1,44 @@
 extends Area2D
 
-# --- ESTAS SON LAS LÍNEAS QUE TE FALTABAN ---
 @export var monedas_necesarias : int = 15
-@export_file("*.tscn") var siguiente_nivel 
-# ---------------------------------------------
+@export_file("*.tscn") var siguiente_nivel
+@export var es_ultimo_nivel : bool = false
 
 var escena_victoria = preload("res://pantalla_victoria/pantalla_victoria.tscn")
+var escena_menu = preload("res://Menu/pantalla_Menu.tscn")
+
 @onready var anim = $AnimatedSprite2D
 
 func _ready() -> void:
-	# Reproduce tu animación de puerta cerrada
 	anim.play("idle_door")
 
 func _on_body_entered(body: Node2D) -> void:
-	# CHIVATO 1: Confirmamos si el juego detecta el choque
-	print("¡ALGO tocó la puerta! Es: ", body.name)
-
 	if body.is_in_group("player"):
-		print("Es el jugador. Monedas actuales: ", Global.monedas)
-		
 		if Global.monedas >= monedas_necesarias:
-			print("¡Abriendo puerta!")
 			call_deferred("abrir_y_finalizar")
 		else:
-			print("Faltan monedas. Necesitas: ", monedas_necesarias)
+			print("Faltan monedas.")
 
 func abrir_y_finalizar():
-	# Animación por código (Tween) porque solo tienes una imagen
+	# ⚠️ SI ES EL ÚLTIMO NIVEL → IR DIRECTO AL MENÚ
+	if es_ultimo_nivel:
+		print("Nivel final. Enviando al menú...")
+		get_tree().change_scene_to_packed(escena_menu)
+		return
+
+	# --- SOLO SI NO ES EL ÚLTIMO NIVEL ---
 	var tween = create_tween()
-	tween.tween_property(anim, "modulate:a", 0.0, 1.0) # Desaparece en 1 seg
-	
+	tween.tween_property(anim, "modulate:a", 0.0, 1.0)
+
 	await tween.finished
 	mostrar_victoria()
 
 func mostrar_victoria():
 	var victoria = escena_victoria.instantiate()
-	victoria.ruta_siguiente_nivel = siguiente_nivel
+
+	if siguiente_nivel != null:
+		victoria.ruta_siguiente_nivel = siguiente_nivel
+	else:
+		print("ADVERTENCIA: no hay siguiente nivel asignado.")
+
 	get_tree().root.add_child(victoria)
